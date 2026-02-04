eb849d9b8408371bd3caf948498f969bac9bf0f6
KrutalevickLev <krutalevichlev@gmail.com>
2026-02-04 18:23:15 +0300

    Task03 work

---
+++ task03/logic.cpp
@@ -0,0 +1,14 @@
+#include "logic.h"
+
+bool is_power_of_two(int number){
+	number = number < 0 ? -number : number;
+
+	while (number >= 1) {
+		number /= 2;
+		if (number == 1) {
+			return true;
+		}
+	}
+
+	return false;
+}
---
+++ task03/logic.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool is_power_of_two(int number);
